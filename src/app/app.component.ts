42a47204da41b61ae9ab8eaf8aede4ceedb0788f
Alonsont <alo.reyesd@duocuc.cl>
2024-06-03 23:31:27 -0400

    correccion de errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from './auth.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  private showMenuPages = ['/home', '/settings', '/servicio-streaming'];
+  private showMenuPages = ['/home', '/settings', '/servicios-streaming'];
   userName: string | null = null;
 
   constructor(private router: Router, private menu: MenuController, private authService: AuthService) {
